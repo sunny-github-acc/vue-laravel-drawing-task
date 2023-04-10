afe22e5fafd8d26a30c68180a4ada5bdb8c612c0
sunny-github-acc <karolis.kazak@gmail.com>
2023-04-10 16:24:55 +0300

    Feat drawing recs and circles

--- frontend/components/Draw.vue
+++ frontend/components/Draw.vue
@@ -1,65 +1,111 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
-  >
-    <div v-if="loadCanvas">
-      <v-stage
-        ref="stage"
-        :config="configKonva"
-        @mousedown="handleMouseDown"
-        @mousemove="handleMouseMove"
-        @mouseup="handleMouseUp"
+  <b-container>
+    <b-row class="vh-100 text-center" align-h="center" align-v="center">
+      <b-card
+        v-if="loadCanvas"
+        title="Draw"
+        tag="title"
+        style="max-width: 20rem"
+        class="mb-2"
       >
-        <v-layer ref="layer">
-          <v-line
-            v-for="line in lines"
-            :key="line.id"
-            :config="{
-              stroke: 'black',
-              points: line.points,
-              strokeWidth: 2,
-            }"
-          />
-          <v-rect
-            v-for="(rec, index) in recs"
-            :key="index"
-            :config="{
-              x: Math.min(rec.startPointX, rec.startPointX + rec.width),
-              y: Math.min(rec.startPointY, rec.startPointY + rec.height),
-              width: Math.abs(rec.width),
-              height: Math.abs(rec.height),
-              fill: 'rgb(0,0,0,0)',
-              stroke: 'black',
-              strokeWidth: 2,
-            }"
-          />
-          <v-circle
-            v-for="(circle, index) in circles"
-            :key="index"
-            :config="{
-              x: Math.min(
-                circle.startPointX,
-                circle.startPointX + circle.width
-              ),
-              y: Math.min(
-                circle.startPointY,
-                circle.startPointY + circle.height
-              ),
-              width: Math.abs(circle.width),
-              height: Math.abs(circle.height),
-              fill: 'rgb(0,0,0,0)',
-              stroke: 'black',
-              strokeWidth: 2,
-            }"
-          />
-        </v-layer>
-      </v-stage>
-    </div>
-    <button v-on:click="changeTool('brush')">Change tool to brush</button>
-    <button v-on:click="changeTool('rec')">Change tool to rectangle</button>
-    <button v-on:click="changeTool('circle')">Change tool to circle</button>
-  </div>
+        <b-card-text>Let's draw something nice! </b-card-text>
+
+        <b-card class="mb-4">
+          <v-stage
+            ref="stage"
+            :config="configKonva"
+            @mousedown="handleMouseDown"
+            @mousemove="handleMouseMove"
+            @mouseup="handleMouseUp"
+          >
+            <v-layer ref="layer">
+              <v-line
+                v-for="(line, index) in lines"
+                :key="index + 'line'"
+                :config="{
+                  stroke: 'black',
+                  points: line.points,
+                  strokeWidth: 2,
+                }"
+              />
+              <v-rect
+                v-for="(rec, index) in recs"
+                :key="index + 'rec'"
+                :config="{
+                  x: Math.min(rec.startPointX, rec.startPointX + rec.width),
+                  y: Math.min(rec.startPointY, rec.startPointY + rec.height),
+                  width: Math.abs(rec.width),
+                  height: Math.abs(rec.height),
+                  fill: 'rgb(0,0,0,0)',
+                  stroke: 'black',
+                  strokeWidth: 2,
+                }"
+              />
+              <v-circle
+                v-for="(circle, index) in circles"
+                :key="index + 'circle'"
+                :config="{
+                  x: Math.min(
+                    circle.startPointX,
+                    circle.startPointX + circle.width
+                  ),
+                  y: Math.min(
+                    circle.startPointY,
+                    circle.startPointY + circle.height
+                  ),
+                  width: Math.abs(circle.width),
+                  height: Math.abs(circle.height),
+                  fill: 'rgb(0,0,0,0)',
+                  stroke: 'black',
+                  strokeWidth: 2,
+                }"
+              />
+            </v-layer>
+          </v-stage>
+        </b-card>
+
+        <div>
+          <b-button
+            :variant="isLine === true ? 'primary' : ''"
+            v-on:click="changeTool('brush')"
+            >Brush</b-button
+          >
+          <b-button
+            :variant="isRec === true ? 'primary' : ''"
+            v-on:click="changeTool('rec')"
+            >Rectangle</b-button
+          >
+          <b-button
+            :variant="isCircle === true ? 'primary' : ''"
+            v-on:click="changeTool('circle')"
+            >Circle</b-button
+          >
+        </div>
+
+        <b-form-group
+          id="fieldset-1"
+          class="mt-4"
+          description="Enter title to save your drawing"
+          label-for="input-1"
+          valid-feedback="Saved!"
+          :invalid-feedback="invalidFeedback"
+          :state="state"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="title"
+            :state="state"
+            @keydown="onChange"
+            trim
+          ></b-form-input>
+        </b-form-group>
+        <b-button class="mt-2" variant="success" v-on:click="save()"
+          >SAVE</b-button
+        >
+      </b-card>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -67,8 +113,26 @@ import Vue from "vue";
 
 export default {
   name: "Draw",
+  computed: {
+    state() {
+      if (!this.showInputError) return null;
+      if (this.title.length > 3 && this.title.length < 26) {
+        return true;
+      }
+      return false;
+    },
+    invalidFeedback() {
+      if (this.title.length < 5) {
+        return "Enter at least 4 characters.";
+      } else if (this.title.length > 25) {
+        return "Enter no more than 25 characters.";
+      }
+    },
+  },
   data() {
     return {
+      title: "",
+      showInputError: false,
       configKonva: {
         width: 200,
         height: 200,
@@ -145,6 +209,7 @@ export default {
       this.isLine = false;
       this.isRec = false;
       this.isCircle = false;
+      this.isDrawing = false;
 
       if (tool === "brush") {
         this.isLine = true;
@@ -153,6 +218,12 @@ export default {
       } else {
         this.isCircle = true;
       }
+    },
+    save() {
+      this.showInputError = true;
+    },
+    onChange() {
+      this.showInputError = false;
     },
   },
 };
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial />
+  <Draw />
 </template>
 
 <script>
